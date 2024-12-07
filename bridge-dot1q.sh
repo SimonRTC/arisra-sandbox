ffb97a0642d7ab92d09f50720f7a4327ea5bf7c8
Simon Malpel <simon.malpel@ovhcloud.com>
2024-12-07 18:49:19 +0100

    add(ovs): Bridges creation and basic interfaces peering

--- bridge-dot1q.sh
+++ bridge-dot1q.sh
@@ -11,3 +11,25 @@ ovs-ofctl add-flow br-int in_port={VM_PORT_BRIDGE},dl_vlan={VM2BRIDGE_TRANSIT_VL
 ovs-ofctl add-flow br-int in_port={EXT_PORT_BRIDGE},dl_vlan={VXLAN_TRANSIT_VLAN},actions=mod_vlan_vid:{VM2BRIDGE_TRANSIT_VLAN},output:{VM_PORT_BRIDGE}
 
 ### dot1q docs: https://developers.redhat.com/blog/2017/06/06/open-vswitch-overview-of-802-1ad-qinq-support
+
+### Tests
+
+# Simulates the processing of a hypothetical packet in the OpenFlow-based forwarding pipeline ("{OVS_PORT_NUMBER}" == "ens4" port number in OvS)
+ovs-appctl ofproto/trace br-ext in_port={OVS_PORT_NUMBER},dl_vlan={VXLAN_TRANSIT_VLAN},dl_dst={EXAMPLE_DESTINATION_MAC}
+
+### Bridges
+
+# Create external bridge (br-ext) with trunk range 100-1124 (up to 1024 vxlan per host)
+ovs-vsctl add-br br-ext
+ovs-vsctl add-port br-ext ens4
+ovs-vsctl set port ens4 vlan_mode=trunk
+ovs-vsctl set port ens4 trunks=100-1124
+
+# Create dot1q bridge (br-dot1q)
+ovs-vsctl add-br br-dot1q
+
+### VPC
+
+# Create internal transit vlan mapping from ToR (leaf) to dot1q-tunnel bridge
+ovs-vsctl add-port br-ext eth0.200 tag=200 -- set interface eth0.200 type=patch options:peer=eth1.200
+ovs-vsctl add-port br-dot1q eth1.200 tag=200 -- set interface eth1.200 type=patch options:peer=eth0.200
